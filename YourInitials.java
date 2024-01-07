703de20614621f065cf74f1e2958a7ca233762bf
rugby8724 <jeremiah.d.wise@gmail.com>
2024-01-07 17:44:34 -0600

    java Challenge One

---
+++ YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("JJJJJ  W   W");
+         System.out.println("  J    W   W");
+         System.out.println("  J    W   W");
+         System.out.println("  J    W W W");
+         System.out.println("  J    WW WW");
+         System.out.println("J J    WW WW");
+         System.out.println(" JJ    W   W");
+         
+    }
+}
